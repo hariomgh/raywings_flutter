218ac45e57e96905d6720b4a4f8bb1939dd9d888
hariomgh <vishwakarmahariom27@gmail.com>
2023-07-18 11:44:11 +0530

    otp screen update

--- lib/otp.dart
+++ lib/otp.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 
 class OtpScreen extends StatefulWidget {
   const OtpScreen({Key? key}) : super(key: key);
@@ -9,6 +12,22 @@ class OtpScreen extends StatefulWidget {
 
 class _OtpScreenState extends State<OtpScreen> {
   Color borderColor = Color(0xFF800000);
+  bool isTimerRunning = false; // Variable to track the timer state
+  int countdownSeconds = 30; // Countdown time in seconds
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer(); // Start the timer when the screen is first loaded
+  }
+
+  @override
+  void dispose() {
+    // Dispose the timer when the widget is disposed
+    timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,37 +38,22 @@ class _OtpScreenState extends State<OtpScreen> {
           padding: const EdgeInsets.all(40),
           child: Center(
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('Verify',
-                style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                )),
-
-                SizedBox(height: 40,),
-
-                Text('we have sent a 4 digit OTP to',
-                    style: TextStyle(
-                      fontSize: 15,
-                      // fontWeight: FontWeight.bold,
-                    )),
-
-                SizedBox(height: 10,),
-
+                Text('Verify', style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
+                SizedBox(height: 40),
+                Text('we have sent a 4 digit OTP to', style: TextStyle(fontSize: 15)),
+                SizedBox(height: 10),
                 Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('+91 *** *** ' + '0000',
-                        style: TextStyle(
-                          fontSize: 20,
-                          // fontWeight: FontWeight.bold,
-                        )),
-
-                    SizedBox(width: 10,),
-
-
-
+                    Text(
+                      '+91 *** *** ' + '0000',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(width: 10),
                     TextButton(
                       style: TextButton.styleFrom(
                         textStyle: TextStyle(
@@ -61,18 +65,16 @@ class _OtpScreenState extends State<OtpScreen> {
                         ),
                       ),
                       onPressed: () {},
-                      
-                      child: Text('Edit Number',
-                      style: TextStyle(
-                        fontSize: 15,
-                      ),
+                      child: Text(
+                        'Edit Number',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
                       ),
                     ),
-                  ]
+                  ],
                 ),
-
-                SizedBox(height: 20,),
-
+                SizedBox(height: 20),
                 OtpTextField(
                   numberOfFields: 5,
                   borderColor: borderColor,
@@ -92,36 +94,80 @@ class _OtpScreenState extends State<OtpScreen> {
                     );
                   },
                 ),
-
-                SizedBox(height: 20,),
-
-                Text('please wait while we verify your OTP'),
-
-
-
-                TextButton(
-                  onPressed: () {},
-                  child: Container(
-                    color: Color(0xff800000),
-                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                    child: const Text(
-                      'S U B M I T',
-                      style: TextStyle(color: Colors.white, fontSize:20 ),
+                SizedBox(height: 20),
+                Visibility(
+                  visible: !isTimerRunning, // Show when the timer is not running
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Didn't receive your otp",
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: TextStyle(
+                            height: 1.2,
+                            fontFamily: 'Dubai',
+                            fontSize: 20,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        onPressed: () {
+                          // Start the timer when "send again" is pressed
+                          startTimer();
+                        },
+                        child: const Text(
+                          'send again',
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Visibility(
+                  visible: isTimerRunning, // Show when the timer is running
+                  child: Text('resend otp in : $countdownSeconds seconds'),
+                ),
+                SizedBox(height: 50),
+                Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    gradient: LinearGradient(
+                      colors: [Colors.black, Color(0xFF800000)],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                    ),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Add your button's action here
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      'SUBMIT',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
                     ),
                   ),
                 ),
-
-                OutlinedButton(
-                  child: Text(
-                    "SUBMIT",
-                    style: TextStyle(
-                      color: Color(0xff800000),
-                      fontSize: 20,
-                    ),
-                  ),
-                  onPressed: () {},
-                ),
-
               ],
             ),
           ),
@@ -129,103 +175,24 @@ class _OtpScreenState extends State<OtpScreen> {
       ),
     );
   }
-}
 
-class OtpTextField extends StatefulWidget {
-  final int numberOfFields;
-  final Color borderColor;
-  final bool showFieldAsBox;
-  final ValueChanged<String>? onCodeChanged;
-  final ValueChanged<String>? onSubmit;
+  void startTimer() {
+    // Start the timer countdown
+    setState(() {
+      isTimerRunning = true;
+    });
 
-  const OtpTextField({
-    Key? key,
-    required this.numberOfFields,
-    required this.borderColor,
-    required this.showFieldAsBox,
-    this.onCodeChanged,
-    this.onSubmit,
-  }) : super(key: key);
-
-  @override
-  _OtpTextFieldState createState() => _OtpTextFieldState();
-}
-
-class _OtpTextFieldState extends State<OtpTextField> {
-  List<TextEditingController> textControllers = [];
-
-  @override
-  void initState() {
-    super.initState();
-    for (int i = 0; i < widget.numberOfFields; i++) {
-      textControllers.add(TextEditingController());
-    }
-  }
-
-  @override
-  void dispose() {
-    for (var controller in textControllers) {
-      controller.dispose();
-    }
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: List.generate(
-        widget.numberOfFields,
-            (index) => Container(
-          width: 40,
-          height: 50,
-          margin: const EdgeInsets.symmetric(horizontal: 5),
-          child: TextField(
-            controller: textControllers[index],
-            onChanged: (value) {
-              if (widget.onCodeChanged != null) {
-                String code = '';
-                for (var i = 0; i < widget.numberOfFields; i++) {
-                  if (i == index) {
-                    code += value;
-                  } else {
-                    code += textControllers[i].text;
-                  }
-                }
-                widget.onCodeChanged!(code);
-              }
-            },
-            onSubmitted: (value) {
-              if (widget.onSubmit != null) {
-                String code = '';
-                for (var controller in textControllers) {
-                  code += controller.text;
-                }
-                widget.onSubmit!(code);
-              }
-            },
-            textAlign: TextAlign.center,
-            keyboardType: TextInputType.number,
-            maxLength: 1,
-            style: TextStyle(fontSize: 20),
-            decoration: InputDecoration(
-              counterText: '',
-              contentPadding: EdgeInsets.zero,
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5),
-                borderSide: BorderSide(color: widget.borderColor, width: 2),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5),
-                borderSide: BorderSide(color: widget.borderColor, width: 2),
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+    timer = Timer.periodic(Duration(seconds: 1), (Timer timer) {
+      setState(() {
+        countdownSeconds--;
+        if (countdownSeconds <= 0) {
+          // Timer finished, reset the variables
+          timer.cancel();
+          isTimerRunning = false;
+          countdownSeconds = 30;
+        }
+      });
+    });
   }
 }
+
